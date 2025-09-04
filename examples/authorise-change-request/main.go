50fd7c111bd244f36c816c7a6d0c4f3f50efafa9
Simon Emms <simon@simonemms.com>
2025-09-04 10:27:13 +0000

    feat: create example auth change request workflow

---
+++ examples/authorise-change-request/main.go
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2025 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+	gh "github.com/mrsimonemms/golang-helpers"
+	"github.com/mrsimonemms/golang-helpers/temporal"
+	"github.com/mrsimonemms/temporal-dsl/pkg/dsl"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"go.temporal.io/sdk/client"
+)
+
+type State struct {
+	ID       uuid.UUID `json:"id"`
+	Approved bool      `json:"approved"`
+	Status   string    `json:"status"`
+}
+
+func exec(isApproved bool, delay time.Duration) error {
+	// The client is a heavyweight object that should be created once per process.
+	c, err := temporal.NewConnection(
+		temporal.WithHostPort(os.Getenv("TEMPORAL_ADDRESS")),
+		temporal.WithNamespace(os.Getenv("TEMPORAL_NAMESPACE")),
+		temporal.WithAPICredentials(os.Getenv("TEMPORAL_API_KEY")),
+		temporal.WithTLS(os.Getenv("TEMPORAL_TLS") == "true"),
+		temporal.WithZerolog(&log.Logger),
+	)
+	if err != nil {
+		return gh.FatalError{
+			Cause: err,
+			Msg:   "Unable to create client",
+		}
+	}
+	defer c.Close()
+
+	workflowOptions := client.StartWorkflowOptions{
+		TaskQueue: "temporal-dsl",
+	}
+
+	ctx := context.Background()
+	we, err := c.ExecuteWorkflow(ctx, workflowOptions, "authoriseChangeRequest", dsl.HTTPData{
+		// Send the reference to the change - might be a DB record or a PR number
+		"changeId": "change-id-ref",
+	})
+	if err != nil {
+		return gh.FatalError{
+			Cause: err,
+			Msg:   "Error executing workflow",
+		}
+	}
+
+	log.Info().Str("workflowId", we.GetID()).Str("runId", we.GetRunID()).Msg("Started workflow")
+
+	quit := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-quit:
+				log.Info().Msg("Workflow completed")
+				return
+			default:
+				res, err := c.QueryWorkflow(ctx, we.GetID(), "", "get_state")
+				if err != nil {
+					// Keep as fatal as in goroutine
+					log.Fatal().Err(err).Msg("Error querying workflow")
+				}
+
+				var state State
+				if err := res.Get(&state); err != nil {
+					// Keep as fatal as in goroutine
+					log.Fatal().Err(err).Msg("Error getting query result")
+				}
+				log.Info().Interface("Query result", state).Msg("Response from query")
+
+				time.Sleep(time.Second * 2)
+			}
+		}
+	}()
+
+	go func() {
+		// Change how long we wait before triggering the signal to cater for the timeout
+		log.Info().Dur("timeout", delay).Bool("isApproved", isApproved).Msg("Waiting before we trigger review")
+		time.Sleep(delay)
+
+		log.Info().Msg("Sending change review response")
+		workflowID := fmt.Sprintf("%s_fork_waitForApproval", we.GetID())
+		if err := c.SignalWorkflow(ctx, workflowID, "", "review", &dsl.HTTPData{
+			// Any data received here is set to the workflow's state
+			"approved": isApproved,
+		}); err != nil {
+			// Fatal error kept in gorouting
+			log.Fatal().Err(err).Msg("Error signalling workflow")
+		}
+	}()
+
+	log.Info().Msg("Waiting for respose")
+	if err := we.Get(ctx, nil); err != nil {
+		return gh.FatalError{
+			Cause: err,
+			Msg:   "Error getting response",
+		}
+	}
+
+	quit <- true
+
+	fmt.Println("===")
+	fmt.Println("Workflow completed")
+	fmt.Println("===")
+
+	return nil
+}
+
+func main() {
+	logLevel := "info"
+	if l, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		logLevel = l
+	}
+
+	level, err := zerolog.ParseLevel(logLevel)
+	if err != nil {
+		os.Exit(gh.HandleFatalError(gh.FatalError{
+			Cause: err,
+			Msg:   "Error parsing log level",
+			WithParams: func(l *zerolog.Event) *zerolog.Event {
+				return l.Str("level", logLevel)
+			},
+		}))
+	}
+	zerolog.SetGlobalLevel(level)
+
+	input := map[string]inputData{
+		"approve": {question: "Do you approve of the change? (Y/n)"},
+		"delay":   {question: "How long do you want to wait until replying? (15s)"},
+	}
+
+	for key, data := range input {
+		answer, err := getInput(data.question)
+		if err != nil {
+			os.Exit(gh.HandleFatalError(err))
+		}
+		data.answer = answer
+		input[key] = data
+	}
+
+	approve := input["approve"].answer == "" || strings.EqualFold(input["approve"].answer, "y")
+
+	delay := time.Second * 15
+	if d := input["delay"].answer; d != "" {
+		var err error
+		delay, err = time.ParseDuration(d)
+		if err != nil {
+			os.Exit(gh.HandleFatalError(err))
+		}
+	}
+
+	if err := exec(approve, delay); err != nil {
+		os.Exit(gh.HandleFatalError(err))
+	}
+}
+
+func getInput(question string) (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(question)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Trim(text, "\n"), nil
+}
+
+type inputData struct {
+	question string
+	answer   string
+}
