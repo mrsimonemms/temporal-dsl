e9544c9340e753612a13dc90e0edee5e555367cb
Simon Emms <simon@simonemms.com>
2025-10-17 08:01:29 +0000

    chore: create scripts directory for local scripts

---
+++ scripts/src/commands/generate-table.mjs
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2025 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import yaml from 'yaml';
+
+export const command = 'generate-table <file> <dir>';
+export const describe = 'Generate the Examples template';
+
+export const builder = (y) =>
+  y
+    .positional('file', {
+      type: 'string',
+      describe: 'Path to readme file',
+    })
+    .positional('dir', {
+      type: 'string',
+      describe: 'Path to examples directory',
+    });
+
+export const handler = async (argv) => {
+  // Get all the data
+  const files = await Promise.all(
+    (await fs.readdir(argv.dir, { withFileTypes: true }))
+      .filter((d) => d.isDirectory())
+      .map(async (d) => {
+        const workflowFile = path.join(argv.dir, d.name, 'workflow.yaml');
+
+        const { document } = yaml.parse(
+          await fs.readFile(workflowFile, 'utf-8'),
+        );
+
+        return {
+          name: `[${document.title ?? document.name}](./${d.name})`,
+          description: document.summary,
+        };
+      }),
+  );
+
+  // Generate the table
+  const table = ['| Name | Description |', '| --- | --- |'];
+
+  files.forEach(({ name, description }) => {
+    table.push(`| ${name} | ${description} |`);
+  });
+
+  const fileContents = await fs.readFile(argv.file, 'utf-8');
+
+  const updated = replaceBetweenAnchors(fileContents, table.join('\n'));
+
+  await fs.writeFile(argv.file, updated);
+};
+
+function replaceBetweenAnchors(content, replacement) {
+  const pattern = /<!-- apps-start -->([\s\S]*?)<!-- apps-end -->/;
+  return content.replace(
+    pattern,
+    `<!-- apps-start -->\n\n${replacement}\n\n<!-- apps-end -->`,
+  );
+}
---
+++ scripts/src/index.mjs
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2025 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+yargs(hideBin(process.argv))
+  .scriptName(process.argv.slice(2))
+  .commandDir('commands', {
+    extensions: ['mjs'],
+    visit: (cmd) => cmd.default ?? cmd,
+  })
+  .usage('Usage: $0 <command> [options]')
+  .demandCommand(
+    1,
+    'Please specify a command. Try --help for a list of commands.',
+  )
+  .recommendCommands()
+  .strict() // only allow defined commands/options
+  .help()
+  .alias('h', 'help')
+  .version()
+  .alias('v', 'version')
+  .fail((msg, err, y) => {
+    if (err) {
+      if (process.env.MYCLI_VERBOSE) console.error('[error]', err);
+      else console.error(err.message || err);
+    } else {
+      console.error(msg);
+    }
+    process.exit(1);
+  })
+  .parse();
