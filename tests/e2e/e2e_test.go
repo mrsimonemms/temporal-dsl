8426ac6292d54a80152ea75f99a2bfe77f2de679
Simon Emms <simon@simonemms.com>
2025-10-25 10:59:09 +0000

    test: create end to end tests

---
+++ tests/e2e/e2e_test.go
@@ -0,0 +1,142 @@
+//go:build e2e
+
+/*
+ * Copyright 2025 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"path"
+	"testing"
+
+	"github.com/mrsimonemms/temporal-dsl/pkg/dsl"
+	"github.com/mrsimonemms/temporal-dsl/tests/e2e/utils"
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/mrsimonemms/temporal-dsl/tests/e2e/tests"
+)
+
+type harness struct {
+	Binary string
+	Cases  []utils.TestCase
+}
+
+var h *harness
+
+func getFreePort() (port int, err error) {
+	var a *net.TCPAddr
+	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
+		var l *net.TCPListener
+		if l, err = net.ListenTCP("tcp", a); err == nil {
+			defer func() {
+				err = l.Close()
+			}()
+			return l.Addr().(*net.TCPAddr).Port, nil
+		}
+	}
+	return port, err
+}
+
+func setup() (*harness, error) {
+	cases := make([]utils.TestCase, 0)
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, c := range utils.GetTestCases() {
+		c.WorkflowPath = path.Join(cwd, "tests", c.Name, c.WorkflowPath)
+
+		workflowDefinition, err := dsl.LoadFromFile(c.WorkflowPath)
+		if err != nil {
+			return nil, err
+		}
+		c.Workflow = workflowDefinition
+		cases = append(cases, c)
+	}
+
+	// Build the binary
+	binaryFile, err := os.MkdirTemp("", "tdsl")
+	if err != nil {
+		return nil, fmt.Errorf("error creating temp file: %w", err)
+	}
+
+	cmd := exec.CommandContext(context.Background(), "go", "build", "-o", binaryFile, "../../")
+	if _, err := cmd.Output(); err != nil {
+		return nil, fmt.Errorf("error building binary: %w", err)
+	}
+
+	return &harness{
+		Binary: path.Join(binaryFile, "temporal-dsl"),
+		Cases:  cases,
+	}, nil
+}
+
+func TestMain(m *testing.M) {
+	testHarness, err := setup()
+	if err != nil {
+		log.Printf("e2e setup failed: %v", err)
+		// Non-zero exit so the test run fails clearly.
+		os.Exit(1)
+	}
+	h = testHarness
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestE2E(t *testing.T) {
+	if h == nil {
+		t.Fatal("harness is nil - setup not run")
+	}
+
+	cancellableCtx := t.Context()
+	defer cancellableCtx.Done()
+
+	for _, test := range h.Cases {
+		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+
+			healthPort, err := getFreePort()
+			assert.NoError(t, err, "health port")
+
+			metricsPort, err := getFreePort()
+			assert.NoError(t, err, "metrics port")
+
+			args := []string{
+				"--file", test.WorkflowPath,
+				"--health-listen-address", fmt.Sprintf("localhost:%d", healthPort),
+				"--metrics-listen-address", fmt.Sprintf("localhost:%d", metricsPort),
+			}
+
+			// Start the Temporal DSL binary with the loaded workflow
+			go (func() {
+				//nolint
+				cmd := exec.CommandContext(cancellableCtx, h.Binary, args...)
+				assert.NoError(t, cmd.Run())
+			})()
+
+			test.Test(t, test)
+		})
+	}
+}
---
+++ tests/e2e/utils/tests.go
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2025 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import (
+	"context"
+	"testing"
+
+	"github.com/mrsimonemms/golang-helpers/temporal"
+	zlog "github.com/rs/zerolog/log"
+	"github.com/serverlessworkflow/sdk-go/v3/model"
+	"github.com/stretchr/testify/assert"
+	"go.temporal.io/sdk/client"
+)
+
+type TestCase struct {
+	Name           string
+	WorkflowPath   string
+	Workflow       *model.Workflow
+	Input          map[string]any
+	ExpectedOutput map[string]any
+	Test           func(t *testing.T, test TestCase)
+}
+
+// RunToCompletion simplest version of the test where it runs to completion and matches the output
+func RunToCompletion(t *testing.T, test TestCase) {
+	c, err := temporal.NewConnectionWithEnvvars(
+		temporal.WithZerolog(&zlog.Logger),
+	)
+	assert.NoError(t, err)
+	defer c.Close()
+
+	workflowOptions := client.StartWorkflowOptions{
+		TaskQueue: test.Workflow.Document.Namespace,
+	}
+
+	wCtx := context.Background()
+
+	we, err := c.ExecuteWorkflow(wCtx, workflowOptions, test.Workflow.Document.Name, test.Input)
+	assert.NoError(t, err)
+
+	var result map[string]any
+	assert.NoError(t, we.Get(wCtx, &result))
+	assert.Equal(t, test.ExpectedOutput, result)
+}
